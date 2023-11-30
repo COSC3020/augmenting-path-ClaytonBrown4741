6338e059327674be586ae88930644f90aba5826c
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-11-30 08:02:20 -0700

    Updated code to look better

--- code.js
+++ code.js
@@ -7,25 +7,18 @@ function augmentingPath(graph, start, end){
 		visitedNodes[keyValues[i]]= Infinity
 	}
 	visitedNodes[source]=0
-	returnedValue=dAlgorithm(graph, start, end);
+	returnedValue=AP(graph, start, end);
 	if (returnedValue==0)
 		return fullPath
 	else if (returnedValue==1)
 		return []
 	else if (returnedValue==2)
 		return []
-	console.log(visitedNodes)
-	//console.log(fullPath)
-	//return fullPath
 }
 
-function dAlgorithm(graph, currentNode, goalNode){
+function AP(graph, currentNode, goalNode){
 	fullPath.push(currentNode)
-	//console.log(fullPath)
-	//console.log(goalNode)
-	//console.log(currentNode)
 	if (goalNode==currentNode){
-		console.log("skdjfksdjfksjdfkd")
 		return 0
 	}
 	allVisited=true;
@@ -38,17 +31,13 @@ function dAlgorithm(graph, currentNode, goalNode){
 		fullPath=[]
 		return 1
 	}
-	smallestWeight=Infinity;
-	smallestEdge=null
+
 	keyValues[currentNode]=Object.keys(graph[currentNode]);
-	//console.log(Object.keys(graph[currentNode]).length)
         for (var j=0; j < Object.keys(graph[currentNode]).length; j++) {
-		console.log(keyValues[currentNode]);
-		console.log(keyValues[currentNode][j])
 		if (visitedNodes[keyValues[currentNode][j]]===Infinity)
 		{
 			visitedNodes[keyValues[currentNode][j]]=1
-			returnValue=dAlgorithm(graph, keyValues[currentNode][j], goalNode)
+			returnValue=AP(graph, keyValues[currentNode][j], goalNode)
 			if (returnValue==2)
 				fullPath.pop()
 			else if (returnValue==1)
@@ -57,20 +46,9 @@ function dAlgorithm(graph, currentNode, goalNode){
 				return 0
 
 		}
-		else
-		{
-			console.log("can't go")
-		}
 	}
 	return 2;
 }
 
 var fullPath=[]
 var visitedNodes=[]
-
-var graph = {'foo': {},
-    'bar': {'foo': 1}};
-
-console.log("Begin")
-console.log(augmentingPath(graph, 'foo', 'bar'));
-console.log("Done")
