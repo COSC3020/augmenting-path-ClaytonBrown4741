dff9648c1b67ce501400835792fb769f905c81d7
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-11-30 17:33:55 -0700

    Updated the code accordingly

--- code.js
+++ code.js
@@ -1,54 +1,46 @@
 function augmentingPath(graph, start, end){
-	fullPath=[]
-	visitedNodes=[]
+	var fullPath=[]
+	var visitedNodes=[]
 	keyValues=Object.keys(graph);
 	source=keyValues[0]
 	for (var i=0; i<Object.keys(graph).length; i++){
 		visitedNodes[keyValues[i]]= Infinity
 	}
 	visitedNodes[source]=0
-	returnedValue=AP(graph, start, end);
-	if (returnedValue==0)
-		return fullPath
-	else if (returnedValue==1)
-		return []
-	else if (returnedValue==2)
-		return []
+	return AP(graph, start, end, fullPath, visitedNodes);
 }
 
-function AP(graph, currentNode, goalNode){
-	fullPath.push(currentNode)
+function AP(graph, currentNode, goalNode, fullPath, visitedNodes){
 	if (goalNode==currentNode){
-		return 0
+		fullPath.push(currentNode)
+		return fullPath
 	}
 	allVisited=true;
 	visitedKeys=Object.keys(visitedNodes)
-	for (var i=0; i<visitedKeys.length; i++){
-		if (visitedNodes[visitedKeys[i]]===Infinity)
-			allVisited=false;
-	}
-	if (allVisited==true){
-		fullPath=[]
-		return 1
+	if (!Object.values(visitedNodes).includes(Infinity)){
+		return []
 	}
 
 	keyValues[currentNode]=Object.keys(graph[currentNode]);
         for (var j=0; j < Object.keys(graph[currentNode]).length; j++) {
 		if (visitedNodes[keyValues[currentNode][j]]===Infinity)
 		{
+			fullPath.push(currentNode)
 			visitedNodes[keyValues[currentNode][j]]=1
-			returnValue=AP(graph, keyValues[currentNode][j], goalNode)
-			if (returnValue==2)
+			// This return value dictates what should happen moving forward  
+			// If an empty set is returned, it knows that the desired answer  
+			// has not been found yet so it knows to keep looking. Otherwise,  
+			// it will return the full path as it will know that the answer  
+			// has been found.
+			returnedValue =  AP(graph, keyValues[currentNode][j], goalNode, fullPath, visitedNodes)
+			if (JSON.stringify(returnedValue)==JSON.stringify([])){
 				fullPath.pop()
-			else if (returnValue==1)
-				return 1
-			else if (returnValue==0)
-				return 0
-
+			}
+			else{
+				return fullPath
+			}
 		}
+		
 	}
-	return 2;
+	return []
 }
-
-var fullPath=[]
-var visitedNodes=[]
